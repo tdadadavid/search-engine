fb978e286b8c3079338bb24e9ba29e1245b73e52
markdave123-py <nwaekwudavid@gmail.com>
2024-08-22 09:24:32 +0100

    merged to main

--- SearchEngine.Api/Core/Cronjobs/jobFactory.cs
+++ SearchEngine.Api/Core/Cronjobs/jobFactory.cs
@@ -7,27 +7,16 @@ using Quartz.Spi;
 
 namespace search.SearchEngine.Api.Core.Cronjobs
 {
-
-
   public class JobFactory : IJobFactory
   {
-
-    private readonly IServiceProvider _serviceProvider;
-
-    public JobFactory(IServiceProvider serviceProvider){
-      _serviceProvider = serviceProvider;
-    }
     public IJob NewJob(TriggerFiredBundle bundle, IScheduler scheduler)
     {
-      //  return Activator.CreateInstance(bundle.JobDetail.JobType) as IJob;
-       return _serviceProvider.GetRequiredService(bundle.JobDetail.JobType) as IJob;
-
+       return Activator.CreateInstance(bundle.JobDetail.JobType) as IJob;
     }
 
     public void ReturnJob(IJob job)
     {
-      // throw new NotImplementedException();
-      (job as IDisposable)?.Dispose();
+      throw new NotImplementedException();
     }
   }
 }
