679fb9bb7d77f9576bff94911247c6ec42b37a69
Williams <williamsnkama@gmail.com>
2024-08-26 13:42:27 +0100

    bug fixes

--- ui/search/src/app/upload/page.tsx
+++ ui/search/src/app/upload/page.tsx
@@ -27,8 +27,8 @@ export default function Page(){
             <form 
             onSubmit={handleSubmit}
             className="mx-auto my-32 p-4 max-w-[500px] bg-slate-800 rounded">
-                <label htmlFor="doc" className="block text-center font-bold hover:cursor-pointer pb-4">Select Document for Upload</label>
-                <input id="doc" type="file" name="doc" className="hover:cursor-pointer block m-auto bg-slate-600 px-3 py-1"/>
+                <label htmlFor="file" className="block text-center font-bold hover:cursor-pointer pb-4">Select Document for Upload</label>
+                <input id="file" type="file" name="file" className="hover:cursor-pointer block m-auto bg-slate-600 px-3 py-1"/>
 
                 <button type="submit" className="block my-2 mx-auto py-2 px-5 rounded-md bg-white hover:bg-slate-600 hover:text-white text-black">Upload</button>
             </form>
