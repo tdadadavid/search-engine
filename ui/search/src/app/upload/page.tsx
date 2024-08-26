13ad031c701238e9801bf622d99c41a093545579
Williams <williamsnkama@gmail.com>
2024-08-26 13:39:50 +0100

    added api

--- ui/search/src/app/upload/page.tsx
+++ ui/search/src/app/upload/page.tsx
@@ -1,9 +1,32 @@
 'use client'
+import { FormEvent } from "react"
 
 export default function Page(){
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        const formdata = new FormData(e.currentTarget)
+
+        try{
+            const reqOpt = {
+                method: "POST",
+                body: formdata,
+            }
+            let response = await fetch("http://localhost:5268/api/documents/upload", reqOpt)
+            if (response.ok){
+                alert("Uploaded successfully")
+            }
+            else{
+                alert("Failed to upload. Try again later!")
+            }
+        } catch(err){
+            console.error(err)
+        }
+    }
     return(
         <main className="">
-            <form className="mx-auto my-32 p-4 max-w-[500px] bg-slate-800 rounded">
+            <form 
+            onSubmit={handleSubmit}
+            className="mx-auto my-32 p-4 max-w-[500px] bg-slate-800 rounded">
                 <label htmlFor="doc" className="block text-center font-bold hover:cursor-pointer pb-4">Select Document for Upload</label>
                 <input id="doc" type="file" name="doc" className="hover:cursor-pointer block m-auto bg-slate-600 px-3 py-1"/>
 
